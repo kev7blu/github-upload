a66d3e8a39d1187bcf4811a71c3cb2b3ab5a90d7
Kevin Henry <kev7blu@gmail.com>
2019-08-29 11:15:57 -0600

    blah learning path

---
+++ blah.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << " ![mobius strip](https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiN5NaRjaHkAhV0IDQIHdEPBDsQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.smithsonianmag.com%2Fscience-nature%2Fmathematical-madness-mobius-strips-and-other-one-sided-objects-180970394%2F&psig=AOvVaw0x4GK_5THXVA_41tgCqo0y&ust=1566928353951364)" << std::endl;
+
+	return 0;	
+}
